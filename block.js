966230390cdc0fa77e4b0781198537c3136267d9
HaarisQadri <72584587+HaarisQadri@users.noreply.github.com>
2020-11-16 08:52:10 -0500

    Update block.js

--- block.js
+++ block.js
@@ -16,7 +16,7 @@ class Block{
         this.Visibility=255;
       }
       display(){
-        if(this.body.speed < 3){
+        if(this.body.speed < 2){
           var angle = this.body.angle;
           push();
           translate(this.body.position.x, this.body.position.y);
